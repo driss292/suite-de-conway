2d30845f49e4d21d08d9776f2ed2a555ab975979
driss292 <drisskaci@outlook.fr>
2024-08-29 17:56:11 +0200

    exo 1 & 2 OK

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,36 @@
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Hello World</h1>
-  </div>
-`;
+function decoupeChaine(str: String) {
+  if (str.length === 0) return "";
+  let result = str[0];
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] !== str[i - 1]) {
+      result += " ";
+    }
+    result += str[i];
+  }
+  return result;
+}
+
+function decritChaine(str: String) {
+  if (str.length === 0) return "";
+  let result = "";
+  let compteur = 1;
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      compteur++;
+    } else {
+      result += compteur + str[i - 1];
+      compteur = 1;
+    }
+  }
+  result += compteur + str[str.length - 1];
+  return result;
+}
+console.log(decoupeChaine("ab")); // a b
+console.log(decoupeChaine("aabbca")); // aa bb c a
+
+console.log(decritChaine("ab")); // 1a1b
+console.log(decritChaine("aabbca")); // 2a2b1c1a
